1d56ecc267054a702b5464cc0b9b8a62c85d7acf
Nikita Konstantinov <110055310+JohnyRico2022@users.noreply.github.com>
2024-03-10 15:46:08 +0300

    added bottom menu

---
+++ app/src/main/java/ru/nikita/weatherappdiplom/ui/MainActivity.kt
@@ -0,0 +1,23 @@
+package ru.nikita.weatherappdiplom.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import ru.nikita.weatherappdiplom.R
+import ru.nikita.weatherappdiplom.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.bottomNavigation
+        val navController = findNavController(R.id.fragment_container)
+
+        navView.setupWithNavController(navController)
+    }
+}
